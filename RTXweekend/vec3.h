2bcf625848b154185660b83ef55cec031886d0d8
kitekii <tim720719@163.com>
2019-09-13 10:33:37 +0800

    add vec3 class

---
+++ RTXweekend/vec3.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include<cmath>
+class vec3
+{
+public:
+	vec3() {};
+	vec3(float e0, float e1, float e2) {
+		e[0] = e0;
+		e[1] = e1;
+		e[2] = e2;
+	}
+	~vec3() = default;
+
+	inline float x() const { return e[0]; }
+	inline float y() const { return e[1]; }
+	inline float z() const { return e[2]; }
+	inline float r() const { return e[0]; }
+	inline float g() const { return e[1]; }
+	inline float b() const { return e[2]; }
+
+	inline const vec3& operator+() const { return *this; }
+	inline vec3& operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+	inline float operator[](int i) const { return e[i]; }
+	inline float& operator[](int i) { return e[i]; }
+
+	inline vec3& operator+=(const vec3 &v2);
+	inline vec3& operator-=(const vec3 &v2);
+	inline vec3& operator*=(const vec3 &v2);
+	inline vec3& operator/=(const vec3 &v2);
+	inline vec3& operator*=(const float t);
+	inline vec3& operator/=(const float t);
+
+	inline float length()const {
+		return sqrt(squared_length());
+	}
+	inline float squared_length() const {
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+	}
+	inline void make_unit_vector();
+
+	float e[3];
+
+};
+
