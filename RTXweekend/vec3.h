83c2c431bf6562eade8117a427ec25435e7b94f4
kitekii <tim720719@163.com>
2019-09-13 10:48:01 +0800

    impl methods

--- RTXweekend/vec3.h
+++ RTXweekend/vec3.h
@@ -36,9 +36,84 @@ public:
 	inline float squared_length() const {
 		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 	}
-	inline void make_unit_vector();
+	inline vec3& normalize();
 
 	float e[3];
 
 };
 
+inline vec3& vec3::normalize() {
+	float k = 1.0 / length();
+	e[0] *= k; e[1] *= k; e[2] *= k;
+	return *this;
+}
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+	return vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+}
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+	return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+}
+inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+	return vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
+}
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+	return vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
+}
+inline vec3 operator*(const vec3 &v1, float t) {
+	return vec3(v1[0] * t, v1[1] * t, v1[2] * t);
+}
+inline vec3 operator*(float t, const vec3 &v1) {
+	return vec3(v1[0] * t, v1[1] * t, v1[2] * t);
+}
+inline vec3 operator/(const vec3 &v1, const float t) {
+	return vec3(v1[0] / t, v1[1] / t, v1[2] / t);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+	return vec3(
+		v1[1] * v2[2] - v1[2] * v2[1],
+		v1[2] * v2[0] - v1[0] * v2[2],
+		v1[0] * v2[1] - v1[1] * v2[0]
+	);
+}
+
+inline vec3& vec3::operator+=(const vec3 &v2){
+	e[0] += v2[0];
+	e[1] += v2[1];
+	e[2] += v2[2];
+	return *this;
+}
+inline vec3& vec3::operator-=(const vec3 &v2){
+	e[0] -= v2[0];
+	e[1] -= v2[1];
+	e[2] -= v2[2];
+	return *this;
+}
+inline vec3& vec3::operator*=(const vec3 &v2){
+	e[0] *= v2[0];
+	e[1] *= v2[1];
+	e[2] *= v2[2];
+	return *this;
+}
+inline vec3& vec3::operator/=(const vec3 &v2){
+	e[0] /= v2[0];
+	e[1] /= v2[1];
+	e[2] /= v2[2];
+	return *this;
+}
+inline vec3& vec3::operator*=(const float t){
+	e[0] *= t;
+	e[1] *= t;
+	e[2] *= t;
+	return *this;
+}
+inline vec3& vec3::operator/=(const float t){
+	e[0] /= t;
+	e[1] /= t;
+	e[2] /= t;
+	return *this;
+}
