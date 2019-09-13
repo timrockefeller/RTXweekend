8dc5090f9311742666aa8b4c491cbb6cd2077fe3
kitekii <tim720719@163.com>
2019-09-13 11:01:45 +0800

    add ray class

---
+++ RTXweekend/ray.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "vec3.h"
+class ray
+{
+public:
+	ray();
+	ray(const vec3& a, const vec3& b) {
+		A = a;
+		B = b;
+	}
+	~ray()=default;
+	
+	vec3 origin() const { return A; }
+	vec3 direction() const { return B; }
+	vec3 pointAt(float t) { return A + t * B; }
+
+	vec3 A;
+	vec3 B;
+};
