7e59159af073059122cf0c10dfb062a9e0d9b061
kitekii <tim720719@163.com>
2019-09-15 15:52:00 +0800

    style util

--- RTXweekend/util.h
+++ RTXweekend/util.h
@@ -40,32 +40,43 @@ namespace RTXW {
 
 		static hitable * random_scene() {
 			int n = 500;
-			hitable **list = new hitable *[n + 1];
+			hitable **list = new hitable*[n + 1];
 			list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertain(vec3(0.5)));
 			int i = 1;
-			for (int a = -11; a < 11; a++) {
-				for (int b = -11; b < 11; b++) {
+			for (int a = -11; a < 11; a += 5) {
+				for (int b = -11; b < 11; b += 5) {
 					float choose_mat = drand();
 					vec3 center(a + 0.9*drand(), 0.2, b + 0.9*drand());
 					if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
-						if (choose_mat < 0.8) {
-							list[i++] = new sphere(center, 0.2, new lambertain(vec3(drand()*drand(), drand()*drand(), drand()*drand())));
-
+						if (choose_mat < 0.8) {  // diffuse
+							list[i++] = new sphere(
+								center, 0.2,
+								new lambertain(vec3(drand()*drand(),
+									drand()*drand(),
+									drand()*drand()))
+							);
 						}
-						else if (choose_mat < 0.95) {
-							list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1 + drand()), 0.5*(1 + drand()), 0.5*(1 + drand())), 0.5*drand()));
-
+						else if (choose_mat < 0.95) { // metal
+							list[i++] = new sphere(
+								center, 0.2,
+								new metal(vec3(0.5*(1 + drand()),
+									0.5*(1 + drand()),
+									0.5*(1 + drand())),
+									0.5*drand())
+							);
 						}
-						else {
+						else {  // glass
 							list[i++] = new sphere(center, 0.2, new dielectric(1.5));
 						}
 					}
 				}
 			}
+
 			list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
 			list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertain(vec3(0.4, 0.2, 0.1)));
 			list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
-			return new hitablelist(list,i);
+
+			return new hitablelist(list, i);
 		}
 	};
 
